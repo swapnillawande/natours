91d86d5998362b25d560b493b4b5786a5b3ea62a
swapnil <swapnillawande2001@gmail.com>
2024-09-07 14:58:58 +0530

    adding the express static for static files

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Middle-Ware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
